6b55506ebb3a2c6bfd1cc94226107a789ce17482
AV <92086489+AnzhelikaVishnevetska@users.noreply.github.com>
2022-01-08 15:54:50 +0100

    Start-menu
    Menu startowe z działającymi przyciskami, z efektem naciśnięcia

---
+++ Project-Eden/Assets/Scripts/menu/buttons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class buttons : MonoBehaviour
+{
+    [SerializeField] Sprite spr;
+    string sc;
+    public void To_Scene(string scene)
+    {
+        GetComponent<Image>().sprite = spr;
+        sc = scene;
+        Invoke("the_scene", .5f);
+    }
+
+    void the_scene()
+    {
+        SceneManager.LoadScene(sc);
+    }
+
+    public void To_Exit()
+    {
+        GetComponent<Image>().sprite = spr;
+        Application.Quit();
+    }
+}
