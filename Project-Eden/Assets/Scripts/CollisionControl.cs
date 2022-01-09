0c63a5f88082cd40f4b18e37214c9326b7193e3f
KubaAngielczyk <kuba.angielczyk@gmail.com>
2022-01-09 17:53:25 +0100

    Skok
    Dodałem skrypt "Jump" pozwalający skakać oraz "CollisionControl" sprawdzający czy gracz znajduje się na ziemi

---
+++ Project-Eden/Assets/Scripts/CollisionControl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionControl : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
